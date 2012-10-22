74d39cec1c35e69b1283d9b54d5e3913deb2ce11
hwrproject <wagner.clemens@gmx.de>
2012-10-22 12:32:18 +0200

    Revert "Problem: Ant.java Ant::nextCity() TODO sum wird infinity"
    This reverts commit 74514aa1f539d112e78400e04660d7ceb1608616.

--- Ants/src/ants/TSP.java
+++ Ants/src/ants/TSP.java
@@ -116,7 +116,7 @@ public class TSP implements Runnable {
     }
 
     public void saveToFile(File file) {
-        if (this.comment == null || this.comment.isEmpty()) {
+        if (this.comment == null) {
             this.comment = JOptionPane.showInputDialog(Main.window, "Bitte geben Sie einen Kommentar ein: ", "Kommentar", JOptionPane.QUESTION_MESSAGE);
         }
         try {
@@ -229,7 +229,6 @@ public class TSP implements Runnable {
     }
     
     public void solveTSP() {
-        //try {
         resetTSP();
 
         for (int i = 0; i < this.iterations; i++) {
@@ -271,10 +270,6 @@ public class TSP implements Runnable {
                 Main.window.refreshPaintPanel();
             }
         }
-        
-        //} catch (Exception e) {
-        //    JOptionPane.showMessageDialog(Main.window, "Während er Berechnung ist ein schwerwiegender Fehler aufgetreten.\n" + e.getMessage(), "Fehler!", JOptionPane.ERROR_MESSAGE);
-        //}
         Main.window.solverFinished();
     }
 
